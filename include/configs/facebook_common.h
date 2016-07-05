a81a33cf628edfe92a599c78dd8b24b9f64fb411
Teddy Reed <teddy@prosauce.org>
2016-07-04 17:21:03 -0700

    Enable SPL and an external keystore for fbyosemite
    This changes the facebook_common configuration to fix SPL-related
    variables for the AST2400. For the FBYosemite board only, a SPL is
    requested and an external FDT is required.
    
    The external FDT contains the keystore for a verified boot.

--- include/configs/facebook_common.h
+++ include/configs/facebook_common.h
@@ -79,25 +79,51 @@
 #define CONFIG_CMD_MEMTEST
 #define CONFIG_CMD_SDRAM
 
+/*
+ * Hash algorithm support
+ */
+#define CONFIG_SHA256
+
 #ifdef CONFIG_SPL
-/* An SPL build expects U-Boot to be in another flash. */
-#define CONFIG_SYS_UBOOT_START		0x24000000
+/*
+ * An SPL build expects U-Boot to be in another flash.
+ * Notice the external flash (0x24000000) includes a 32kB offset (0x8000).
+ * This allows mkimage to build a FIT, with a max size of 32kB to proceed the
+ * firmware blob.
+ */
+#define CONFIG_SYS_UBOOT_START		0x24008000
+
+/* Define the base address to search for a FIT within the SPL. */
+#define CONFIG_SYS_SPL_FIT_BASE		0x24000000
+/* This is an in-development change required for mkimage. */
+#define CONFIG_SPL_LOAD_FIT_OFFSET	0x00008000
 #ifdef CONFIG_SPL_BUILD
-#define CONFIG_SPL_MAX_FOOTPRINT	0x10000
+#define CONFIG_SPL_FRAMEWORK
+#define CONFIG_SPL_MAX_FOOTPRINT	0x100000
+/* During an SPL build the base is 0x0. */
 #define CONFIG_SYS_TEXT_BASE		0x00000000
 #define CONFIG_SPL_TEXT_BASE		CONFIG_SYS_TEXT_BASE
-#define CONFIG_SPL_STACK		(CONFIG_SYS_SRAM_BASE + 0x3000)
-#define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_SRAM_BASE + 0x3000)
-#ifdef CONFIG_SYS_MALLOC_F_LEN
-#undef CONFIG_SYS_MALLOC_F_LEN
-#define CONFIG_SYS_MALLOC_F_LEN		0x2000
-#endif
+/* Grow the stack down from 0x6000 to an expected max of 12kB. */
+#define CONFIG_SPL_STACK		(CONFIG_SYS_SRAM_BASE + 0x6000)
+#define CONFIG_SYS_INIT_SP_ADDR		CONFIG_SPL_STACK
+/* Establish an 'arena' for heap from 0x1000 - 0x3000, 8k */
+#define CONFIG_SYS_SPL_MALLOC_START	(CONFIG_SYS_SRAM_BASE + 0x1000)
+#define CONFIG_SYS_SPL_MALLOC_SIZE	0x2000 /* 8kB */
+
+/* General SPL build feature includes. */
 #define CONFIG_SPL_DISPLAY_PRINT
 #define CONFIG_SPL_LIBGENERIC_SUPPORT
 #define CONFIG_SPL_LIBCOMMON_SUPPORT
 #define CONFIG_SPL_SERIAL_SUPPORT
+
+/* Verified boot required features. */
+#define CONFIG_SPL_CRYPTO_SUPPORT
+#define CONFIG_SPL_HASH_SUPPORT
+#define CONFIG_SPL_SHA256
+/* This will increase binary size by +10kB */
 #define CONFIG_FIT_SPL_PRINT
 #else
+/* During the U-Boot build the base address is the SPL FIT start address. */
 #define CONFIG_SYS_TEXT_BASE		CONFIG_SYS_UBOOT_START
 #endif
 #endif
